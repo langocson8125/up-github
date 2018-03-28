b6a5416a4f3102e121bee0c7ce0f7c0c7fcc86e9
La Ngoc Son <langocson8125@gmail.com>
2018-03-28 23:09:03 +0700

    Update CapPhatMangHaiChieu.cpp

--- CapPhatMangHaiChieu.cpp
+++ CapPhatMangHaiChieu.cpp
@@ -4,30 +4,36 @@
 #include "math.h"
 #include "limits.h"
 
-//THAO TAC DE QUY VOI MANG HAI CHIEU CO SU DUNG CON TRO
+//THAO TÁC ĐỆ QUY TRÊN MẢNG HAI CHIỀU CÓ SỬ DỤNG CON TRỎ
+// Lỗi xảy ra: tràn bộ nhớ
 
+// hàm cấp phát động mảng hai chiều
 void CapPhat(int **array, int dong, int cot){
-	array = (int *)realloc(0, sizeof(int *) * dong);
+	array = (int **)realloc(0, sizeof(int *) * dong);
+	// cấp phát số dòng (tương đương mỗi ô nhớ)
 	
-	for(int i = 0; i < dong; i++){
-		(array + i) = (int *)realloc(0, sizeof(int *) * cot);
+	for(int i = 0; i < dong; i++){ // cấp phát số cột
+		array[i] = (int *)realloc(array[i], sizeof(int) * cot);
 	}
 }
 
 int main(){
-	int dong, cot;
+	int dong, cot; // input dòng và cột
 	do{
 		printf("\nNhap so dong, so cot cua mang: ");
 		scanf("%d %d", &dong, &cot);
+		printf("%d %d", dong, cot);
 		if(dong < 0 || cot < 0){
 			printf("\nNhap sai, nhap lai.");
 		}
 	}
 	while(dong < 0 || (dong > 0 && cot < 0));
 	
+	// khai báo con trỏ cấp 2
 	int **array;
-	CapPhat(array, dong, cot);
-	// giai phong mang a
+	CapPhat(array, dong, cot);  // thực hiện hàm cấp phát
+	
+	// giải phóng mảng sau khi sử dụng
 	free(array);
 	return 0;
 }
