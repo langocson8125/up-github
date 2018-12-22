7856eb0993632b512a45f4b469f9cfd02c6f2746
La Ngoc Son <langocson8125@gmail.com>
2018-12-22 10:46:25 +0700

    Update TableData.java

--- JavaTrainging/Homework02/ThongTinSinhVien/TableData.java
+++ JavaTrainging/Homework02/ThongTinSinhVien/TableData.java
@@ -43,7 +43,7 @@ public class TableData extends JFrame {
 		this.QLSV.readFileData();
 		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 919, 417);
+		setBounds(100, 100, 884, 291);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
